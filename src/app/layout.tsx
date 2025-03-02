2b5710b0acdec2aa5713f45a332fa181d92a0513
wano-rojano <cbrojano@up.edu.ph>
2025-03-02 20:37:08 +0800

    feat: Routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white p-4 text-center">
+          {/*<p>Welcome to Next.js 15 Crash Course</p>*/}
+          <Navigation />
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white p-4 text-center">
+          Codevolution
+        </footer>
       </body>
     </html>
   );
