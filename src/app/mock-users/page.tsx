59f757703a89176ab9cbb76bd0faa6e17f2b001c
wano-rojano <cbrojano@up.edu.ph>
2025-03-02 21:31:50 +0800

    feat: Server Actions

---
+++ src/app/mock-users/page.tsx
@@ -0,0 +1,58 @@
+import { revalidatePath } from "next/cache";
+
+type MockUser = {
+  id: number;
+  name: string;
+};
+
+export default async function Users() {
+  const res = await fetch("https://67c4597ac4649b9551b35655.mockapi.io/users");
+  const users = await res.json();
+
+  async function addUser(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const res = await fetch(
+      "https://67c4597ac4649b9551b35655.mockapi.io/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      },
+    );
+    const newUser = await res.json();
+    console.log(newUser);
+    revalidatePath("/mock-users");
+  }
+
+  return (
+    <div className="py-10">
+      <form action={addUser} className="mb-4">
+        <input
+          type="text"
+          name="name"
+          required
+          className="p-2 mr-2 border border-gray-300 rounded text-gray-700"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Add User
+        </button>
+      </form>
+      <div className="grid grid-cols-4 gap-4 ">
+        {users.map((user: MockUser) => (
+          <div
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
